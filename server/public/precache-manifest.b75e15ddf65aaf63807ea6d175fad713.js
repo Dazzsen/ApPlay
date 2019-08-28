31cf2fd4435e5acc695bbcdd3f90c6a29b946823
Dazzsen <and24_21@msn.com>
2019-08-28 16:35:22 +0200

    a ver2

---
+++ server/public/precache-manifest.b75e15ddf65aaf63807ea6d175fad713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f8fc29fab676a6b52d4e760620b1e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f5aa99f9c7363893173",
+    "url": "/static/css/main.657b28f6.chunk.css"
+  },
+  {
+    "revision": "a80f7d0516891e49ae88",
+    "url": "/static/js/2.36ec62e2.chunk.js"
+  },
+  {
+    "revision": "6f5aa99f9c7363893173",
+    "url": "/static/js/main.522945f8.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "ec473ed428a3918e56aa99830cb953ef",
+    "url": "/static/media/fondo.ec473ed4.jpg"
+  }
+]);
