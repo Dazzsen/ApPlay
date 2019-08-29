2591f298c4f19d3f5ba73a2b8e5ef3d3ee37ca8e
Dazzsen <and24_21@msn.com>
2019-08-29 10:30:13 +0200

    29

--- server/websockets.js
+++ server/websockets.js
@@ -1,12 +1,31 @@
 const socketio = require("socket.io");
+const Room = require("./models/Room")
 
 module.exports = server => {
   const io = socketio(server);
-  io.on("connection", function (socket) {
+  io.on("connection", (socket) => {
     console.log("a user connected");
-    socket.on("mensajeria", m => {
-      console.log(`NUEVO MENSAJE: ${m}`);
-      socket.broadcast.emit("front", m);
-    });
+
+    socket.on('subscribe', (room) => {
+      console.log('joining room', `${room} ${room}`);
+      // socket.join(`${room.room} ${room.num}`);
+  });
+  socket.on('mensajeria', ({room, msg}) => {
+      console.log(room)
+      console.log('sending room post', `${room} ${room}`);
+      // io.sockets.in(`${room.room} ${room.num}`).emit('front!', {
+      //     message: msg.message || msg
+      // });
+  });
+
+
+
+
+
+
+    // socket.on("mensajeria", m => {
+    //   console.log(`NUEVO MENSAJE: ${m}`);
+    //   socket.broadcast.emit("front", m);
+    // });
   });
 };
